b69d9ab15214f3a57488060c48d43d6ce07bf0bd
Vansh bhatt <144117143+Vanshbhattok@users.noreply.github.com>
2025-11-27 11:55:22 +0530

    Added more operations

--- src/addition.py
+++ src/addition.py
@@ -6,3 +6,32 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+    
+def sub(a, b):
+    return a - b
+
+def test_sub():
+    assert sub(1, 2) == -1
+    assert sub(1, -1) == 2
+    
+def mult(a, b):
+    return a * b
+
+def test_mult():
+    assert mult(1, 2) == 2
+    assert mult(1, -1) == -1
+    
+def div(a, b):
+    return a / b
+
+def test_div():
+    assert div(1, 2) == 0.5
+    assert div(1, -1) == -1
+
+def mod(a, b):
+    return a % b
+
+def test_div():
+    assert mod(1, 2) == 1
+    assert mod(1, -1) == 1
+
