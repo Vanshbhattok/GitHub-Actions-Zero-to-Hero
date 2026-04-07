813041fbb822b68fb20b7f469c1b68b3e27fc7f3
Vansh bhatt <144117143+Vanshbhattok@users.noreply.github.com>
2026-04-07 18:33:20 +0530

    Remove div and mod functions with associated tests
    Removed division and modulus functions and their tests.

--- src/addition.py
+++ src/addition.py
@@ -21,17 +21,5 @@ def test_mult():
     assert mult(1, 2) == 2
     assert mult(1, -1) == -1
     
-def div(a, b):
-    return a / b
 
-def test_div():
-    assert div(1, 2) == 0.5
-    assert div(1, -1) == -1
-
-def mod(a, b):
-    return a % b
-
-def test_div():
-    assert mod(1, 2) == 1
-    assert mod(1, -1) == 1
 
